b2e4ceabc78736a6db63d00c237c95a708e52253
Klaas Nebuhr <klaas.nebuhr@gmail.com>
2019-07-05 08:57:37 +0200

    School and Course

---
+++ digicubes/storage/models/school.py
@@ -0,0 +1,17 @@
+from tortoise import fields
+
+from .support import BaseModel, NamedMixin
+
+class School(NameMixin, BaseModel):
+
+    class Meta:
+        # pylint: disable=too-few-public-methods
+        # pylint: disable=missing-docstring
+        table = "school"
+
+
+class Course(NameMixin, BaseModel):
+
+    from_date = fields.DateField()
+    until_date = fields.DateField()
+    is_private = fields.BooleanField(default=False)    
