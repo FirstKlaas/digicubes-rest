139348c2ea978e643707507e62bcea34cf4c1452
Klaas Nebuhr <klaas.nebuhr@gmail.com>
2019-07-08 08:45:44 +0200

    dev utils

---
+++ devutil/documentation.py
@@ -0,0 +1,9 @@
+from tortoise import Tortoise
+
+
+def generate_model_documentation():
+    """
+    Generates a *.rst file for each model registered in Tortoise ORM
+    which documents the Fields and Relations.
+    """
+    pass
