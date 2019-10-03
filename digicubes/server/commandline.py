2b90a6c2feb09e6020bf4ec941a2896c3ce28e52
Klaas Nebuhr <nebuhr@encoway.de>
2019-10-03 15:03:00 +0200

    commandline

--- digicubes/server/commandline.py
+++ digicubes/server/commandline.py
@@ -1,6 +1,7 @@
 """
 Usage:
     digicubes run
+    digicubes setup
     digicubes --version
 
 Options:
@@ -27,6 +28,12 @@ def evaluate_command():
     print(arguments)
     if arguments.get("run", False):
         run()
+    elif arguments.get("setup", False):
+        arguments.pop("run")
+        try:
+            setup(arguments)
+        except KeyboardInterrupt:
+            logger.info("\nSetup was canceled by user. No configuration file has been created.")
 
 
 def run():
@@ -44,8 +51,32 @@ def run():
 
     server.run()
 
-def setup():
+
+def setup(arguments):
     """
     Creates an initial setup.
     """
+
+    def _read_port():
+        port = None
+        while port is None:
+            port = input("Port to be used [3000]: ")
+            if port == "":
+                port = 3000
+            else:
+                try:
+                    port = int(port)
+                except ValueError:
+                    logger.info("Please enter a number.")
+        return port
+
+    print(arguments)
     configpath = os.environ.get("DIGICUBES_CONFIG_PATH", "cfg")
+    if os.path.exists(configpath) and os.path.isdir(configpath):
+        logger.info("Configuration directory exists. Good. Using '%s'.", configpath)
+    else:
+        logger.info("Configuration directory does not exist.")
+        logger.info("Creating directory '%s'' for you.", configpath)
+        os.makedirs(configpath)
+
+    print(_read_port())
---
+++ setup.py
@@ -0,0 +1,62 @@
+"""
+Setup file for the digicubes package
+"""
+from setuptools import setup, find_packages
+
+def version() -> str:
+    """
+    Returns the current version of the digicubes server
+    """
+    return "0.0.4"
+
+def requirements() -> list:
+    """
+    Returns an array of required packages für the server.
+    """
+    return open("requirements.txt", "rt").read().splitlines()
+
+setup(
+    # Application name:
+    name="digicubes-server",
+    # Version number:
+    version=version(),
+    # Application author details:
+    author="Klaas Nebuhr, Marion Nebuhr",
+    author_email="klaas.nebuhr@gmail.com",
+    # License
+    license="Apache License Version 2.0",
+    # Entry Points
+    entry_points={
+        'console_scripts' : [
+            'start_digicubes=digicubes.server.commandline:run',
+            'digicubes=digicubes.server.commandline:evaluate_command'
+        ]
+    },
+    # Packages
+    packages=find_packages(
+        include=["digicubes*"]
+    ),
+    zip_safe=True,
+    # Include additional files into the package
+    include_package_data=False,
+    # Details
+    description="The digicubes api server",
+    long_description=open("README.rst", "r").read(),
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Framework :: AsyncIO",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Operating System :: OS Independent",
+    ],
+    keywords=(
+        "rest digicubes api learning platform"
+    ),
+    # Dependent packages (distributions)
+    install_requires=requirements(),
+)
