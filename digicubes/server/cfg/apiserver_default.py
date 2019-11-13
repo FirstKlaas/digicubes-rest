94e55103cc6fc1a03a3da4fbf6eef7f6375c08c3
Klaas Nebuhr <klaas.nebuhr@gmail.com>
2019-11-13 16:04:08 +0100

    Default conf file as python module

---
+++ digicubes/server/cfg/apiserver_default.py
@@ -0,0 +1,36 @@
+"""
+Defaults for the api server
+"""
+
+SIMPLE_SETTINGS = {
+  "OVERRIDE_BY_ENV": True,
+  "CONFIGURE_LOGGING": True
+}
+
+# Limit the number of records returned in a response.
+request = {
+  "max_count": 100,
+  "default_count": 10
+}
+
+# Enable the standard frontend.
+web_app = {
+  "mount": False,
+  "route": "/web"
+}
+
+# Enable graphql. This is highly experimental
+# Don't activate it in production environment.
+graphql = {
+  "mount": False,
+  "route": "/graphql"
+}
+
+# The port, ther server is listening on.
+port = 3000
+
+# Database URL
+db_url = "sqlite://digicubes.db"
+
+# Secret to be used to encrypt data.
+secret = "b3j6casjk7d8szeuwz00hdhuw4ohwDu9o"
