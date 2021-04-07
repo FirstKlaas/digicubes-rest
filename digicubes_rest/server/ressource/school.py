3ae1d2832d1997581214c9b8a4f9b7cebf9b009f
Klaas Nebuhr <nebuhr@encoway.de>
2021-04-07 17:30:27 +0200

    Fixed bug in updating a school.

--- digicubes_rest/server/ressource/school.py
+++ digicubes_rest/server/ressource/school.py
@@ -75,7 +75,7 @@ class SchoolRessource(BasicRessource):
 
         try:
             school: models.School = await models.School.get(id=school_id)
-            school.update(data)
+            school.update_from_dict(data)
             school.modified_at = datetime.utcnow()
             await school.save()
             # filter_fields = self.get_filter_fields(req)
